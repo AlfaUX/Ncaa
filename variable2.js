68ede679749b303209cf37bba9664594acc50f8b
jihannkn <khanzajihan@gmail.com>
2023-11-05 08:45:04 +0700

    update

---
+++ variable2.js
@@ -0,0 +1,70 @@
+// const star = 10;
+// for(let a = star; a > 0; a--){
+//     let space = ` `;
+//     for (let i = 1; i <= a; i++){
+//         space += ` `;
+//     }
+//     let asterisk = ` `;
+//     for (let a1 = star; a1 >= a; a1--){
+//         asterisk += `*`;
+//     }
+//     console.log(space + asterisk);
+// }
+
+// const star = 10;
+// for(let a = star; a > 0; a--){
+// let asterisk = ` `;
+// for (let b = star; b >= a; b--){
+//     asterisk += `*`;
+// }
+// console.log(asterisk);
+// }
+
+// const star = 10;
+// for (let a = 1; a <= star; a++){
+//     let space = ` `;
+//     for (let b = 1; b <= a; b++){
+//         space +=` `
+//     }
+//     let asterisk = ` `;
+//     for (let c = star; c >= a; c-- )(
+//         asterisk += `*`;
+//     )
+//     console.log (space + asterisk);
+// }
+
+// const rows = 5;
+// let output = ` `;
+
+// // membuat segitiga bagian atas 
+// for (let i = 1; i<= ResizeObserver; i++){
+//     for (let j = 1; j <= rows - i; j++){
+//         output += ` `;
+//     }
+//     for (let k = 1; k <= * 2 - 1; k++)
+//     output += `\n`;
+// }
+// // membuat segitiga bagian bawah
+// for (let i = rows - 1; i >= 1; i--){
+//     for (let j = 1; j <= rows - i; j++){
+//         output += ` `;
+//     }
+//     for (let k = 1; k <= i * 2 - 1; k++){
+//         output += `*`;
+//     }
+//     output += `\n`
+// }
+// console.log(output);
+
+const star = 10;
+for (let a = star; a > 0 ; a--){
+    let space = ` `;
+    for (let i = 1; i <= a; i++){
+        space += ` `;
+    }
+    let asterisk = ` `;
+    for (let a1 = star; a1 >= a; a1--){
+        asterisk += `*`;
+    }
+    console.log(space + asterisk);
+}
